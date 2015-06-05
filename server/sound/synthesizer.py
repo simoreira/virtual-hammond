bae4d2b52fb732bb0fa881e2f25a126c07856bf5
Miguel Silva <maps@ua.pt>
2015-06-05 19:03:29 +0100

    organized modules

--- server/sound/synthesizer.py
+++ server/sound/synthesizer.py
@@ -1,28 +1,41 @@
 from math import sin, pi
-import sys
 
 class Synthesizer(object):
-	def synthesize(self, organ_registry_string, interpreter_list):
+	def __init__(self, interpreter_list, organ_registry):
+		self.interpreter_list = interpreter_list
+		self.durations = get_durations(interpreter_list)
+		self.frequencies = get_frequencies(interpreter_list)
+		self.organ_registry = get_organ_registry(organ_registry)
+
+	def get_durations(self, interpreter_list):
+		durations = []
+		for i in range(0, len(interpreter_list)):
+			durations.append(interpreter_list[i][0])	
+		return durations
+
+	def get_frequencies(self, interpreter_list):
+		frequencies = []
+		for i in range(0, len(interpreter_list)):
+			frequencies.append(interpreter_list[i][1])	
+		return frequencies
+
+	def get_organ_registry(self, organ_registry):
+		organ_registry_list = []
+		for value in organ_registry:
+			organ_registry_list.append(int(value))
+		return organ_registry_list
+
+	def synthesize(self, organ_registry_string):
 		'''
 		Method responsible for producing a sequence of samples
 		that will represent the wave form of each note.
 		'''
-		organ_reg_int = []
-		for i in range(0, len(organ_registry_string)):
-			tmp = list(organ_registry_string)
-			organ_reg_int.append(int(tmp[i]))
 
-		duration = []
-		frequencies = []
 		output_samples = []
 		output = []
 
 		for i in range(0, len(interpreter_list)):
-			duration.append(interpreter_list[i][0])
-			frequencies.append(interpreter_list[i][1])
-
-		for i in range(0, len(interpreter_list)):
-			output_samples.append(self.get_samples(organ_reg_int, duration[i], frequencies[i]))
+			output_samples.append(self.get_samples(organ_reg_int, self.durations[i], self.frequencies[i]))
 
 		normalized_output_samples = self.normalize(output_samples)
 
@@ -44,24 +57,20 @@ class Synthesizer(object):
 		mult_frequencies = self.get_mult_freq(frequency)
 
 		for i in range(0, int(rate*duration)):
-			data.append(
-				(org_reg_list[0]/8 * sin(2*pi*mult_frequencies[0]*i/rate)) +
-				(org_reg_list[1]/8 * sin(2*pi*mult_frequencies[1]*i/rate)) +
-				(org_reg_list[2]/8 * sin(2*pi*mult_frequencies[2]*i/rate)) +
-				(org_reg_list[3]/8 * sin(2*pi*mult_frequencies[3]*i/rate)) +
-				(org_reg_list[4]/8 * sin(2*pi*mult_frequencies[4]*i/rate)) +
-				(org_reg_list[5]/8 * sin(2*pi*mult_frequencies[5]*i/rate)) +
-				(org_reg_list[6]/8 * sin(2*pi*mult_frequencies[6]*i/rate)) +
-				(org_reg_list[7]/8 * sin(2*pi*mult_frequencies[7]*i/rate)) +
-				(org_reg_list[8]/8 * sin(2*pi*mult_frequencies[8]*i/rate))
-			)
+			data.append(self.get_one_sample(org_reg_list, mult_frequencies, rate, i))
 		return data
+
+	def get_one_sample(self, org_reg_list, mult_frequencies, rate, i):
+		value = 0
+		for j in range(0, len(org_reg_list)):
+			value += org_reg_list[j]/8 * sin(2*pi*mult_frequencies[j]*i/rate)
+		return value
 
 	def get_mult_freq(self, freq):
 		values = [1/2.0, 2/3.0 , 1, 2, 3, 4, 5, 6, 8]
 		data = []
-		for i in range(0, 9):
-			data.append(freq*values[i])
+		for value in values:
+			data.append(value*freq)
 		data = map(int, data)
 		return data
 
@@ -88,3 +97,9 @@ class Synthesizer(object):
 				tmp.append(values * normalize_factor)
 			normalized_data.append(tmp)
 		return normalized_data
+
+if __name__ == "__main__":
+	a = Synthesizer()
+	b = [8,8,8,8,8,8,8,8,8]
+	print(a.get_mult_freq(440))
+	print(a.get_samples(b,1,440))
--- server/sound/wav_generator.py
+++ server/sound/wav_generator.py
@@ -1,23 +1,20 @@
 import wave
-from sound.effects import Effects
+from sound.rtttl_parser import RtttlParser
+from sound.synthesizer import Synthesizer
+from sound.effects_processor import EffectsProcessor
 from struct import pack
-from math import sin, pi
 
 class WavGenerator(object):
-    def __init__(self, filename, tones, rate = 44100, effects = ['none']):
-        self.tones    = tones
-        self.rate     = rate
-        self.effects  = effects
+    def __init__(self, rtttl, effects = []):
+        self.rtttl_parser = RtttlParser(rtttl)
+        self.synthesizer = Synthesizer(rtttl_parser.interpret())
+        self.effects_processor = EffectsProcessor(synthesizer.synthesize(), effects)
 
     def create(self, filename):
         wav = wave.open(filename, 'w')
         wav.setparams((1, 2, 44100, 0, 'NONE', 'not compressed'))
 
-        data = []
-
-        for tone in tones:
-            if 'none' in effects:
-                data = effect_none(data, tone['samples'])
+        data = effects_processor.process()
 
         wav_data = ''
         for v in data:
@@ -25,3 +22,7 @@ class WavGenerator(object):
 
         wav.writeframes(wav_data)
         wav.close()
+
+if __name__ == '__main__':
+    gerador = WavGenerator('Dragon Ball GT:d=4,o=6,b=140:p,c,c,8a5,8a5,8c,8d,c,a5,a5,g5,a5,a5,8g5,8a5,8a5,a5,g5,f5,e5,p,8d5,8d5,f5,d,f5,8g5,8a5,a5,a5,g5,f5,g5,p,f5,e5,f5')
+    gerador.create("teste.wav")
