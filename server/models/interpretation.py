9a5f626063cb88d13b26b11b948b607897f2c0af
faviouz <fabiu.maia@gmail.com>
2015-06-07 01:58:31 +0100

    edit models

--- server/models/interpretation.py
+++ server/models/interpretation.py
@@ -1,6 +1,9 @@
 from models.base_model import BaseModel
 
 class Interpretation(BaseModel):
+    def __init__(self, database):
+        self.database = database
+
     def create_interpretation(self, data):
         if not data['song_id'] == None and not data['registry'] == None and not data['effects'] == None:
             self.database.query('INSERT INTO interpretations(song_id, registry, effects) VALUES(?, ?, ?)', (data['song_id'], data['registry'], data['effects']))
@@ -13,17 +16,14 @@ class Interpretation(BaseModel):
         interpretation = self.database.fetch('SELECT * FROM interpretations WHERE id=?', (id,))
         return interpretation
 
+    def get_interpretation_votes_by_id(self, id):
+        interpretation = self.database.fetch('SELECT votes FROM interpretations WHERE id=?', (id,))
+        return interpretation[0]['votes']
+
     def get_interpretations_by_song_id(self, song_id):
         interpretations = self.database.fetch('SELECT * FROM interpretations WHERE song_id=?', (song_id,))
         return interpretations
 
-    def update_interpretation_by_id(self, id, data):
-        if not data['song_id'] == None:
-            self.database.query('UPDATE interpretations SET song_id=? WHERE id=?', (data['song_id'], id))
-        if not data['registry'] == None:
-            self.database.query('UPDATE interpretations SET registry=? WHERE id=?', (data['registry'], id))
-        if not data['effects'] == None:
-            self.database.query('UPDATE interpretations SET effects=? WHERE id=?', (data['effects'], id))
-
-    def delete_interpretation_by_id(self, id):
-        self.database.query('DELETE FROM interpretations WHERE id=?', (id,))
+    def get_interpretations_wave_files_by_song_id(self, song_id):
+        wave_files = self.database.fetch('SELECT wave_file FROM interpretations WHERE song_id=?', (song_id,))
+        return wave_files
--- server/models/song.py
+++ server/models/song.py
@@ -1,6 +1,9 @@
 from models.base_model import BaseModel
 
 class Song(BaseModel):
+    def __init__(self, database):
+        self.database = database
+
     def create_song(self, data):
         self.database.query('INSERT INTO songs(rtttl) VALUES(?)', (data['rtttl'],))
 
@@ -13,12 +16,12 @@ class Song(BaseModel):
         return song
 
     def get_song_rtttl_by_id(self, id):
-        song = self.database.fetch('SELECT rtttl FROM songs WHERE id=?', (id,))
-        return song['rtttl']
+        rtttl = self.database.fetch('SELECT rtttl FROM songs WHERE id=?', (id,))
+        return rtttl[0]['rtttl']
 
-    def get_song_wave_form_by_id(self, id):
-        song = self.database.fetch('SELECT wave_form FROM songs WHERE id=?', (id,))
-        return song['wave_form']
+    def get_song_wave_form_file_by_id(self, id):
+        song = self.database.fetch('SELECT wave_form_file FROM songs WHERE id=?', (id,))
+        return song[0]['wave_form_file']
 
     def update_song_by_id(self, id, data):
         if not data['rtttl'] == None:
