c9d986e4d1486e2d08344a853fd00f5e322d60ba
Ana Cruz <anapatriciacruz@ua.pt>
2015-06-07 21:53:32 +0100

    web interface work

--- client/scripts/add.js
+++ client/scripts/add.js
@@ -3,20 +3,51 @@ $(document).ready(function() {
         e.preventDefault();
 
         var rtttl_string = $('#rtttl').val();
+        var song_id;
 
         $.ajax({
             method: 'POST',
             url: '/api/create_song',
             data: { rtttl: rtttl_string }
-        }).done(function(data) {
+        })
+        .done(function(data) {
+            console.log(data);
+
             if(data.success)
             {
-                console.log(data);
+                song_id = data.data.id;
+            }
+            else
+            {
+                $('.form').prepend('<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation-triangle"></i>Error<p>Invalid song information.</p></div>');
+            }
+        });
+
+        var registry = $('#registry').val();
+        var effects = [];
+
+        $("input:checkbox:checked").each(function() {
+            effects.push($(this).val());
+        });
+
+        // Prepare array into CSV for AJAX request
+        effects = effects.join(',');
+
+        $.ajax({
+            method: 'POST',
+            url: '/api/create_interpretation',
+            data: { song_id: song_id, registry: registry, effects: effects }
+        })
+        .done(function(data) {
+            console.log(data);
+
+            if(data.success)
+            {
                 window.location = '/songs';
             }
             else
             {
-                $('.form').prepend('<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation-triangle"></i>Error<p>Invalid RTTTL string.</p></div>');
+                $('.form').prepend('<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation-triangle"></i>Error<p>Invalid song information.</p></div>');
             }
         });
     });
--- client/scripts/songs.js
+++ client/scripts/songs.js
@@ -1,10 +1,44 @@
+var interpret = function(id) {
+    $('.song__interpret--' + id).show();
+
+    $.ajax({
+        method: 'POST',
+        url: '/api/create_interpretation',
+        data: {song_id: id, registry: registry, effects: effects}
+    }).done(function(data) {
+        console.log(data);
+    });
+};
+
+var getInterpretations = function(id) {
+    $.ajax({
+        method: 'GET',
+        url: '/api/list_song_files',
+        data: { id: id }
+    }).done(function(data) {
+        if(data.success && data.length > 0)
+        {
+            $('.song__interpretations--' + id).show();
+
+            console.log(data);
+            var wave_files = data.data.wave_files;
+
+            wave_files.forEach(function(wave_file) {
+                $('#song__interpretation').append('<div><audio controls><source src="' + wave_file['wave_file'] + '" type="audio/wav"></audio></div>');
+            });
+        }
+        else
+        {
+            $('#song__interpretation--' + id).append('<div class="alert alert-danger" role="alert"><i class="fa fa-exclamation-triangle"></i>Error<p>No interpretations for this song.</p></div>');
+        }
+    });
+};
+
 $(document).ready(function() {
     $.ajax({
         method: 'GET',
         url: '/api/list_songs'
     }).done(function(data) {
-        console.log(data.data.songs);
-
         var songs = data.data.songs;
 
         new Vue({
@@ -15,7 +49,32 @@ $(document).ready(function() {
         });
     });
 
+    var interpret = function(song_id) {
+        if($('.song__interpret--'+song_id).is(":visible"))
+        {
+            $('.song__interpret--'+song_id).show();
+        }
+        else
+        {
+            $('.song__interpret--'+song_id).hide();
+        }
+
+        $('/api/create_interpretation', {song_id: song_id, registry: registry, effects: effects}, function(data) {
+            console.log(data);
+        });
+
+        console.log(id)
+    }
+
     $('#interpret').on('click', function(e) {
+        e.preventDefault();
+
+        console.log('banana');
+
+        window.location = '/interpret';
+    });
+
+    $('#interpretations').on('click', function(e) {
         e.preventDefault();
 
         window.location = '/interpret';
